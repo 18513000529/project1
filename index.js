f17b0289831b347abd0332831e02c35841a7da6f
18513000529 <454706364@qq.com>
2018-06-15 08:40:37 +0800

    在分支2上修改了index.js文件

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 var oLi = document.getElementsByTagName('li');
+var obox = document.getElementById('box');
 for(var i=0;i<oLi.length;i++){
-    oli[i].onclick=function(){
-        console.log(1)
+    oLi[i].index= i;
+    oLi[i].onclick=function(){
+        for(var j=0;j<oLi.length;j++){
+            oLi[j].calssName='';
+        }
+        oLi[this.index].className='active';
+        for(var a = 0;a<obox.length;a++){
+        	obox[a].style.display="none";
+        }
+        obox[this.index].style.display='block';
     }
 }
